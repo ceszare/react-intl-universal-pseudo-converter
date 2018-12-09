93d1ee68ee2f946edae3a04bd77b57bc6c111830
Cesar Zapata <community@ceszare.com>
2018-12-09 10:57:19 -0800

    Adds JSDoc documentation to the transformer

--- src/transformer.js
+++ src/transformer.js
@@ -1,5 +1,12 @@
 const { pseudoTranslate } = require('icu-pseudo-translate');
 
+/**
+ * A utility that iterates through a tree and returns a new copy of the tree, where all the
+ * leaf nodes of type `string` are transformed through the pseudoTranslate utility.
+ *
+ * @param node - A reference to the root node of the tree.
+ * @returns - A reference to the root node of a new tree, in which string leaf nodes have been pseudo-localized.
+ */
 function transform_tree(node) {
   if (typeof node === 'string') {
     return pseudoTranslate(node);
